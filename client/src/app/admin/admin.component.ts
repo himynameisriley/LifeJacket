a467b4723b15a8328a1f2b8755b30c3a96ccd764
braydenrobbins <brayden.robbins@ruralsource.com>
2020-02-14 11:33:02 -0600

    added the admin service and created a route to add a step, and added a form to the admin template to add the step

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../admin.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private adminService: AdminService) { }
+
+  addSuccess: boolean = false;
+  addFailure: boolean = false;
 
   ngOnInit() {
+  }
+
+  hideAlert() {
+    this.addFailure = false;
+    this.addSuccess = false;
+  }
+
+  addStep(title: string, url: string, description: string): void {
+    const data = { title, url, description }
+    this.adminService.AddStep(data).subscribe(
+      result => {
+        console.log('success', result);
+        this.addSuccess = true;
+      },
+      error => {
+        console.log(error);
+        this.addFailure = true;
+      }
+    );
   }
 
 }
